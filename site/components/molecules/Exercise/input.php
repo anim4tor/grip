91d75c89eadeb14621c73ba162a152b089a2126a
anim4tor <jiri.klusak@gmail.com>
2025-08-30 14:07:05 +0200

    Checkbox fix

--- site/components/molecules/Exercise/input.php
+++ site/components/molecules/Exercise/input.php
@@ -3,22 +3,22 @@
 	$exercise ??= null;
 	extract($exercise);
 ?>
-<exercise class="grid relative" modal-reveal data-checkbox>
-	<label for="<?= $id ?>" class="grid">
-		<input type="checkbox" id="<?= $id ?>" name="exercises[]" value="<?= $id ?>" <?= e($checked, 'checked') ?>/>
-		<div class="flex justify__space-between align__start inner__1" checkbox>
-			<header class="flex gap__1 align__center">
-				<div class="grid" >
-					<div class="exercise__figure grid__stack place__center-center" >
+<exercise class="grid relative" modal-reveal >
+	<div class="flex justify__space-between align__start inner__1" >
+		<header class="flex gap__1 align__center">
+			<div class="grid" data-checkbox>
+				<label for="<?= $id ?>" class="grid">
+					<input type="checkbox" id="<?= $id ?>" name="exercises[]" value="<?= $id ?>" <?= e($checked, 'checked') ?>/>
+					<div class="exercise__figure grid__stack place__center-center" checkbox>
 						<div class="button checkbox border bg__invert/10" ><icon class="op__0"><?= svg('public/assets/images/ui/ui_checkmark.svg') ?></icon></div>
 					</div>
-				</div>
-				<div class="grid gap__02 align__center">
-					<h2 class="inner-r__8"><?= $name ?></h2>
-				</div>
-			</header>
-		</div>
-	</label>
+				</label>
+			</div>
+			<div class="grid gap__02 align__center">
+				<h2 class="inner-r__8"><?= $name ?></h2>
+			</div>
+		</header>
+	</div>
 	<nav class="absolute inset__top-right button__group inner-t__1">
 		<div class="button circle op__5"><icon><?= svg('public/assets/images/ui/ui_chevron-right.svg') ?></icon></div>
 	</nav>
